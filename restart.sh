3c1619f621f7899b826a5a1178ce33bd181621a1
Frank <frank.yu@dazong.com>
2018-03-30 14:59:57 +0800

    * 优化改进

--- restart.sh
+++ restart.sh
@@ -8,8 +8,8 @@ docker rm phpservice
 
 # 重新生成
 echo 'restart phpservice container...'
-# docker run --name phpservice -p 8080:80 -v /web/docker/conf/php/php.ini:/usr/local/etc/php/php.ini -v /web/docker/conf/nginx/nginx.conf:/etc/nginx/nginx.conf -v /web/docker/conf/nginx/sites-enabled:/etc/nginx/sites-enabled/ -v /web/docker/wwwroot:/var/www/html -d frankyu/phpservice
-docker run --name phpservice -p 8080:80 -d frankyu/phpservice
+docker run --name phpservice -p 8080:80 -v /web/docker/conf/php/php.ini:/usr/local/etc/php/php.ini -v /web/docker/conf/nginx/nginx.conf:/etc/nginx/nginx.conf -v /web/docker/conf/nginx/sites-enabled:/etc/nginx/sites-enabled/ -v /web/docker/wwwroot:/var/www/html -d frankyu/phpservice
+# docker run --name phpservice -p 8080:80 -d frankyu/phpservice
 echo 'start php-fpm...'
 docker exec -idt phpservice php-fpm
 echo 'cur container ps...'
